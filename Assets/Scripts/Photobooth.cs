36ee5add471ab093ce56ba9dca8b795a823be3ec
Shantanu Maskeri <shaan.mask@gmail.com>
2022-01-08 23:45:37 +0400

    AR Photobooth
    Interactive photo booth using face tracking and AR

---
+++ Assets/Scripts/Photobooth.cs
@@ -0,0 +1,31 @@
+﻿using OpenCVForUnity.UnityUtils.Helper;
+using System.IO;
+using UnityEngine;
+
+public class Photobooth : MonoBehaviour
+{
+
+    public WebCamTextureToMatHelper CamTextureToMatHelper;
+
+    private int snapCount = 0;
+
+    private void Start()
+    {
+        PlayerPrefs.DeleteAll();
+        snapCount = PlayerPrefs.GetInt("snapCount");
+    }
+
+    public void SavePhotoToDisk()
+    {
+        snapCount++;
+        PlayerPrefs.SetInt("snapCount", snapCount);
+
+        string directory = Application.dataPath + "/../Photos/";
+
+        if (!Directory.Exists(directory))
+            Directory.CreateDirectory(directory);
+
+        ScreenCapture.CaptureScreenshot(directory + "snap_" + snapCount + ".jpg");
+    }
+
+}
